bf0d03e63c9f9cc20e3007beb1438ebf940fbaaa
Nitin Malhotra <nitinmalhotra1314@gmail.com>
2025-08-12 22:13:07 +0530

    Added 2787: Ways to express an integer as sum of powers

---
+++ Medium/2787-Ways_to_express_an_integer_as_sum_of_powers.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numberOfWays(int n, int x) {
+        const int MOD = 1e9 + 7;
+        vector<int> dp(n + 1, 0);
+        dp[0] = 1;
+        
+        for (int a = 1; ; ++a) {
+            long long ax = 1;
+            for (int i = 0; i < x; ++i) ax *= a; 
+            if (ax > n) break;
+            
+            for (int i = n; i >= ax; --i) {
+                dp[i] = (dp[i] + dp[i - ax]) % MOD;
+            }
+        }
+        return dp[n];
+    }
+};
