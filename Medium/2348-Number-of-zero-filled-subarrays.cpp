9847ec160da5a8df6ffb3a5ef1f2fca078878fa2
Nitin Malhotra <nitinmalhotra1314@gmail.com>
2025-08-19 22:23:08 +0530

    Added 2348: Number of zero filled subarrays

---
+++ Medium/2348-Number-of-zero-filled-subarrays.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    long long zeroFilledSubarray(vector<int>& nums) {
+        long long totalSubarrays = 0, currentStreak = 0;
+        for (int num : nums) {
+            if (num == 0) {
+                currentStreak++;
+                totalSubarrays += currentStreak;
+            } else {
+                currentStreak = 0;
+            }
+        }
+        return totalSubarrays;
+    }
+};
