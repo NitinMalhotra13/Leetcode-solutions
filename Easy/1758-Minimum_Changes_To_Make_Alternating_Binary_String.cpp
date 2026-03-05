44dcb10c7b9d317ebca362c508524a4857ceecdc
Nitin Malhotra <nitinmalhotra1314@gmail.com>
2026-03-05 22:10:07 +0530

    Implement solution for minimum changes in binary string

---
+++ Easy/1758-Minimum_Changes_To_Make_Alternating_Binary_String.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minOperations(string s) {
+        
+        int n = s.size();
+        int p1 = 0, p2 = 0;
+        
+        for(int i = 0; i < n; i++) {
+            
+            if(s[i] != (i % 2 ? '1' : '0'))
+                p1++;
+            
+            if(s[i] != (i % 2 ? '0' : '1'))
+                p2++;
+        }
+        
+        return min(p1, p2);
+    }
+};
