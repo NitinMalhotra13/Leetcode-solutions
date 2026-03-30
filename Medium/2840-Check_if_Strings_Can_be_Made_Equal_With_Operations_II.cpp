c688fe2dcb8a7fb166311d2b4e2b2f98f7956f3e
Nitin Malhotra <nitinmalhotra1314@gmail.com>
2026-03-30 22:52:13 +0530

    Implement string equality check with operations

---
+++ Medium/2840-Check_if_Strings_Can_be_Made_Equal_With_Operations_II.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool checkStrings(string s1, string s2) {
+        int cnt[2][26] = {};
+
+        for (int i = 0; i < s1.size(); i++) {
+            cnt[i % 2][s1[i] - 'a']++;
+            cnt[i % 2][s2[i] - 'a']--;
+        }
+
+        for (int i = 0; i < 2; i++)
+            for (int j = 0; j < 26; j++)
+                if (cnt[i][j]) return false;
+
+        return true;
+    }
+};
