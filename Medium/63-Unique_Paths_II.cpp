d7062f7bb731c6af86bb0bfb53593b390252af4c
Nitin Malhotra <nitinmalhotra1314@gmail.com>
2026-03-31 22:51:56 +0530

    Implement uniquePathsWithObstacles function

---
+++ Medium/63-Unique_Paths_II.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& g) {
+        int m = g.size(), n = g[0].size();
+        vector<int> dp(n, 0);
+
+        dp[0] = (g[0][0] == 0);
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (g[i][j] == 1) {
+                    dp[j] = 0;
+                } else if (j > 0) {
+                    dp[j] += dp[j - 1];
+                }
+            }
+        }
+        return dp[n - 1];
+    }
+};
