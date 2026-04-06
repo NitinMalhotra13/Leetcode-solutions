e52ed229679ef92a3eb61ce964deb65750b8cb4b
Nitin Malhotra <nitinmalhotra1314@gmail.com>
2026-04-06 22:11:09 +0530

    Implement robot return to origin solution

---
+++ Easy/657-Robot_Return_to_Origin.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    bool judgeCircle(string s) {
+        return count(s.begin(),s.end(),'U') == count(s.begin(),s.end(),'D') &&
+               count(s.begin(),s.end(),'L') == count(s.begin(),s.end(),'R');
+    }
+};
