cabdd6ddb2f4f9b937f899858259a19bd34f395c
Nitin Malhotra <nitinmalhotra1314@gmail.com>
2026-07-20 12:15:15 +0530

    Implement topKFrequent function to find top K elements

---
+++ Medium/347-Top_K_Frequent_Elements.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int,int> freq;
+
+        for (int x : nums){
+            freq[x]++;
+        }
+
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> minheap;
+
+        for (auto it : freq){
+            minheap.push({it.second,it.first});
+
+            if (minheap.size() > k){
+                minheap.pop();
+            }
+        }
+
+        vector<int> result;
+
+        while (!minheap.empty()){
+            result.push_back(minheap.top().second);
+            minheap.pop();
+        }
+        return result;
+    }
+};
